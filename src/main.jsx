6e56b3637504b0294f9a3c09e63d2da68735d559
Arafat <sakib15-5422@diu.edu.bd>
2023-06-08 14:38:19 +0600

    footer added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { router } from './Routes/Routes.jsx'
+import { router } from './Routes/Routes'
+import {
+  
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
