8f2e32e8382b39bbe51ac462fbb1c7dbd83d06b2
Arafat <sakib15-5422@diu.edu.bd>
2023-06-08 15:15:10 +0600

    footer updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { router } from './Routes/Routes'
 import {
-  
+
   RouterProvider,
 } from "react-router-dom";
 
@@ -16,6 +16,8 @@ import {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto bg-slate-50'>
     <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
