143cd817d9fdceaa92fd5a8df11a373ab3d67b60
Arafat <sakib15-5422@diu.edu.bd>
2023-06-08 23:13:03 +0600

    login updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,34 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+const handleLogin =  event =>{
+  event.preventDefault();
+  form = event.target;
+  const email = form.email.value;
+  const password = form.password.value;
+console.log(email, password)
+}
+
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col ">
+        <div className="hero min-h-screen bg-white text-black ">
+  <div className="hero-content flex-col   rounded">
     <div className="text-center ">
       <h1 className="text-5xl font-bold">Please Login</h1>
       
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+    <div className="card  w-full max-w-lg shadow-2xl bg-base-100 ">
+      <form onSubmit={handleLogin} className="card-body bg-orange-300  rounded">
         <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
+          <label className="label ">
+            <span className="label-text font-semibold text-black">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text font-semibold text-black">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          <input type="password" name='password' placeholder="password" className="input  input-bordered" />
+          
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          
+          <input className="btn btn-primary text-orange-700" type="submit" value="login" />
         </div>
-      </div>
+        <div>
+          New to Yoku? please <Link to="/register">register</Link>
+        </div>
+      </form>
     </div>
   </div>
 </div>
