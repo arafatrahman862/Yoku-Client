ef6e511ead47f31f5efbab4dca71a4991a7447ba
Arafat <sakib15-5422@diu.edu.bd>
2023-06-13 03:12:56 +0600

    darkmood added

--- src/pages/SelectedClass.jsx
+++ src/pages/SelectedClass.jsx
@@ -36,7 +36,7 @@ const SelectedClass = () => {
           })
     }
     return (
-        <div className='grid grid-cols-3 my-12'>
+        <div className='grid grid-cols-2 gap-6'>
             {
                 joinedClasses.map(({ _id, img, instructor_name, class_name, available_seat, price, }) => <div className="card card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src={img} /></figure>
